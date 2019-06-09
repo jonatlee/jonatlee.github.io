d90d903229c7f46fdf9f5c02623136ae95437282
jonatlee <jonathanlee457@gmail.com>
2019-06-09 00:54:28 -0600

    setup react

---
+++ src/components/app/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Home from '../pages/Home';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <main>
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                </Switch>
+            </main>
+        );
+    }
+}
+
+export default Main;
