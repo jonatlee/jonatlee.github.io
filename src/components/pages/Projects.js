5db418d936b389451110bed64542a5f12c584f7e
jonatlee <jonathanlee457@gmail.com>
2019-06-14 23:03:24 -0600

    projects initial concept

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,14 +1,98 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
+import {Container, Row, Col, Button} from 'react-bootstrap';
 
 class Projects extends React.Component {
     render() {
         return (
-            <Container>
+            <Container style={{marginBottom: 25}}>
                 <div className="project-header">
                     Projects
                 </div> 
                 <hr className="hline" />
+                <Container className="projects-container" style={{marginBottom: 0}}>
+                    <Row style={{paddingBottom: 0}}>
+                        <Col>
+                            <Container className="projects-proj"> 
+                                <div className="projects-proj-name">
+                                    NBA RankSVM
+                                </div>
+                                <div className="projects-proj-desc">
+                                    Implemented RankSVM learning algorithm 
+                                    to predict NBA team standings based on raw team statistics, such
+                                    as points, assists, and rebounds.
+                                </div>
+                                <div className="projects-proj-langs">
+                                    <Button className="projects-proj-langs-btn">
+                                        Python
+                                    </Button> 
+                                </div>
+                            </Container>
+                        </Col>
+                        <Col>
+                            <Container className="projects-proj">
+                                <div className="projects-proj-name">
+                                    Flexbox Calculator
+                                </div>
+                                <div className="projects-proj-desc">
+                                    Created a basic scientific calculator
+                                    using CSS flexbox. Supports up to
+                                    10 digits of precision.
+                                </div>
+                                <br />
+                                <div className="projects-proj-langs">
+                                    <Button className="projects-proj-langs-btn">
+                                        HTML
+                                    </Button> 
+                                    <Button className="projects-proj-langs-btn off">
+                                        CSS
+                                    </Button> 
+                                    <Button className="projects-proj-langs-btn off">
+                                        JavaScript
+                                    </Button> 
+                                </div>
+                            </Container>
+                        </Col>
+                    </Row>
+                    <Row style={{paddingTop: 0, paddingBottom: 20}}>
+                        <Col>
+                            <Container className="projects-proj"> 
+                                <div className="projects-proj-name">
+                                    SideStepper Game
+                                </div>
+                                <div className="projects-proj-desc">
+                                    Used Java Swing GUI to create a basic sidestepper game where 
+                                    the player avoids falling anvils and collects coins to score.
+                                </div>
+                                <br />
+                                <div className="projects-proj-langs">
+                                    <Button className="projects-proj-langs-btn">
+                                        Java
+                                    </Button> 
+                                </div>
+                            </Container>
+                        </Col>
+                        <Col>
+                            <Container className="projects-proj">
+                                <div className="projects-proj-name">
+                                    Zyphel
+                                </div>
+                                <div className="projects-proj-desc">
+                                    A text RPG game that I am currently developing.
+                                </div>
+                                <br />
+                                <br />
+                                <div className="projects-proj-langs">
+                                    <Button className="projects-proj-langs-btn">
+                                        C++
+                                    </Button> 
+                                    <Button className="projects-proj-langs-btn off">
+                                        Still Being Developed
+                                    </Button> 
+                                </div>
+                            </Container>
+                        </Col>
+                    </Row>
+                </Container>
             </Container> 
         );
     }
