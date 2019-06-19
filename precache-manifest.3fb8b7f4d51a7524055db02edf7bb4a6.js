d4bbe2c0f12d5610d70b4573c380e3a78be82efb
jonatlee <jonathanlee457@gmail.com>
2019-06-19 17:40:34 -0600

    Updates

---
+++ precache-manifest.3fb8b7f4d51a7524055db02edf7bb4a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee63a025c16f897834034675c0e3c0fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e7f888586ce33d097c7",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a7663f4465f9e26df9b2",
+    "url": "/static/css/main.65dfb2df.chunk.css"
+  },
+  {
+    "revision": "8e7f888586ce33d097c7",
+    "url": "/static/js/2.d37fdc16.chunk.js"
+  },
+  {
+    "revision": "a7663f4465f9e26df9b2",
+    "url": "/static/js/main.f181ad39.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e2506a519162930116bc7204f2f44e5a",
+    "url": "/static/media/about.e2506a51.jpg"
+  },
+  {
+    "revision": "049ee5ad1c869ba61fea2aea9fba42e3",
+    "url": "/static/media/resume.049ee5ad.pdf"
+  },
+  {
+    "revision": "3c0de2561ca5d7c3fece633ebee0c546",
+    "url": "/static/media/stock.3c0de256.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.5de6ade694fadc607b8525167aaaef4b.js"
+  "/precache-manifest.3fb8b7f4d51a7524055db02edf7bb4a6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
